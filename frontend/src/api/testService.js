0e5b5df0378ebc6e25ceaa92e2f9ce5019dacf50
Pierre <pierre.hamonic71@gmail.com>
2025-04-17 01:12:46 +0200

    Suppression ESlint

--- frontend/src/api/testService.js
+++ frontend/src/api/testService.js
@@ -1,35 +1,35 @@
-import { ajax } from 'rxjs/ajax';
+import { ajax } from "rxjs/ajax";
 
-const API_URL = 'http://localhost/alsia/backend'; //process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = "http://localhost/alsia/backend"; //process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
 
 /**
  * Service class for managing projects from the API.
  */
 class TestService {
-    /**
-     * Allows to get a project from the database.
-     * 
-     * @param {*} projetId The project identifier.
-     */
-    getTest = () => {
-        const url = `${API_URL}/tests`;
-        // const headers = { 'l': this.login, 't': this.token, 'Accept-Language': this.langueId };
-        
-        return ajax.get(url); //, headers);
-    };
+  /**
+   * Allows to get a project from the database.
+   *
+   * @param {*} projetId The project identifier.
+   */
+  getTest = () => {
+    const url = `${API_URL}/tests`;
+    // const headers = { 'l': this.login, 't': this.token, 'Accept-Language': this.langueId };
 
-    /**
-     * Update the definition of the project
-     * 
-     * @param {*} projetId The project identifier
-     * @param {*} body the data to save
-     */
-    // updateDefinitionProject = (projetId, body) => {
-    //     const url = `${this.rootApiUrl}/projects/${projetId}/definition`;
-    //     const headers = { 'l': this.login, 't': this.token, 'Content-Type': 'application/json', 'Accept-Language': this.langueId };
+    return ajax.get(url); //, headers);
+  };
 
-    //     return ajax.patch(url, body, headers);
-    // };
+  /**
+   * Update the definition of the project
+   *
+   * @param {*} projetId The project identifier
+   * @param {*} body the data to save
+   */
+  // updateDefinitionProject = (projetId, body) => {
+  //     const url = `${this.rootApiUrl}/projects/${projetId}/definition`;
+  //     const headers = { 'l': this.login, 't': this.token, 'Content-Type': 'application/json', 'Accept-Language': this.langueId };
+
+  //     return ajax.patch(url, body, headers);
+  // };
 }
 
 export default TestService;
